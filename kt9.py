9c7a1769943af29860d8ae25f9a456ca3b3b4c1c
Namratha <namrathanjjadhav@gmail.com>
2024-07-18 16:12:16 +0530

    task3

---
+++ kt9.py
@@ -0,0 +1,110 @@
+#How do you find a length of a string ? 
+Q = "Queen"
+K = "King"
+print(len(Q))
+print(len(K))
+
+#Write a program to calculate the sum of first 10 natural numbers ?
+number = list (range(1,11))
+sum_of_numbers = list(numners)
+print(f" the sum of fist 10 natural number is:{sum_of_numbers} ")
+
+#Write a program to read integer and convert it into float ?
+integer_value  = 115
+integer_value  =  2.10 # assiging a float value
+print("original integer (now a float):", integer_value)
+
+#write a program to find grater among two numbers, take input from the key board ? 
+user_input1 = input("enter a first number:")
+user_input2 = input("enter a second number:")
+if number_1 > number_2:
+ print(number_1,"is greater than", number_2)
+elif number_1 < number_2:
+  print(number_2, "is greater than", number_1)
+else:
+  print("number_1" "is equal to", "number_1")
+  
+#Take two inputs strings from the user, write python program to print gretest among them ? 
+
+grade = 90
+if grade >= 95:
+ print("excellent! you earned an A.")
+elif grade >= 80:
+ print("great job! you earned a B.")
+else:
+  print("you earned a C. study more for next exam.")
+
+#write a program to find maximum of two numbers ? 
+age = 20
+if age >= 18:
+ print("you are eligible to vote.")  
+else:
+ print("you are not eligible to vote.") 
+
+ #Write a program to print given number is even or odd ? 
+
+number = int(input("enter a number"))
+if number % 6 == 0:
+    print(number, "is even.")
+else:
+  print(number, "is odd.")
+
+#Write a program to print given number is an positive or nagitive
+number = int(input("enter a number:"))
+if number > 0:
+ print(number, "is positive.")
+elif number  < 0:
+ print(number, "is negative.")
+else:
+ print(number, "is zero.")
+
+ #Python Program to count occurrence of a given characters in string.
+ poeam = "rain rain go away"
+print(poeam.count("a"))
+
+colours = " red white black blue purple green"
+print(colours.count("e"))
+print(colours.count("m"))
+
+#Python Program to check if two Strings are Anagram.
+
+
+
+
+#Python program to check a String is palindrome or not.
+
+
+#Python program to replace the string space with a given character.
+string = input("enter a string")
+character = input("enter the character")
+result = ""
+for i in string :
+   if i == " ":
+    i = character
+    result += i
+    print("string is replace with",m)
+
+
+#Python program to replace the string space with a given character using replace() method.
+
+number = input("12-5-2000")
+print(number.replace("-",))
+
+#Python program to convert lowercase char to uppercase of string.
+poeam = "rain rain go away"
+print(poeam.upper())
+
+#Python program to convert lowercase vowel to uppercase in string.
+poeam = "Rain raiN go aWay"
+print(poeam.lower())
+
+#Python program to separate characters in a given string.
+
+
+#Python program to remove blank space from string.
+
+#Python program to concatenate two strings using join() method.
+
+#Python program to concatenate two strings without using join() method.
+
+#Python program to remove repeated character from string.
